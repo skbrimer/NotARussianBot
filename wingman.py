a148811d4a636e7257db7d0aedb30f90493accd0
skbrimer <skbrimer@gmail.com>
2019-02-10 13:56:52 -0600

    changes to the settings

---
+++ wingman.py
@@ -0,0 +1,9 @@
+import settings
+import tweepy
+
+auth = tweepy.OAuthHandler(settings.consumer_key, settings.consumer_secret)
+auth.set_access_token(settings.access_token, settings.access_token_secret)
+api = tweepy.API(auth)
+
+user = api.me()
+print (user.name)
