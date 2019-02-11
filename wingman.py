7ba1921d234a27cf59c13b97a4019027e0e5082c
skbrimer <skbrimer@gmail.com>
2019-02-10 19:58:41 -0600

    Starting over

--- wingman.py
+++ wingman.py
@@ -5,5 +5,5 @@ auth = tweepy.OAuthHandler(settings.consumer_key, settings.consumer_secret)
 auth.set_access_token(settings.access_token, settings.access_token_secret)
 api = tweepy.API(auth)
 
-user = api.me()
-print (user.name)
+user = api.get_user("Andrewdexter1")
+print(user)
