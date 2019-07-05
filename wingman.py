36304d8addec90e3d59345cfb1c482a94f2780bf
skbrimer <skbrimer@gmail.com>
2019-07-05 14:43:02 -0500

    made it a script and not function nad now it works, wondering what I am missing

--- wingman.py
+++ wingman.py
@@ -1,6 +1,13 @@
 import random
 import tweepy
-from config import create_api
+import settings
+
+
+auth = tweepy.OAuthHandler(settings.consumer_key, settings.consumer_secret)
+auth.set_access_token(settings.access_token, settings.access_token_secret)
+
+api = tweepy.API(auth, wait_on_rate_limit = True,
+                    wait_on_rate_limit_notify = True)
 
 Messages = ["You got this Sugar Bear",
     "That is a gun in my pocket, but I am happy to see you.",
@@ -12,15 +19,6 @@ Messages = ["You got this Sugar Bear",
     "It’s just a flesh wound",
     "I learned a long time ago that worrying is like a rocking chair. It gives you something to do but it doesn’t get you anywhere"]
 
-def wingman(api):
-    user_id = "307187231"
-    message = random.choice(Messages)
-    api.update_status(f"@{user_id}, {message}")
-
-
-def main():
-    api = create_api()
-
-
-if __name__ == "__main__":
-    main()
+user_id = "AndrewDexter1"
+message = random.choice(Messages)
+api.update_status(f"@{user_id}, {message}")
