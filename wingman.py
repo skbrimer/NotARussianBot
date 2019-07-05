d12ce38d21cd21d09707e8a8e904f84016fb9550
skbrimer <skbrimer@gmail.com>
2019-07-05 14:36:09 -0500

    starting with daily affirmations

--- wingman.py
+++ wingman.py
@@ -1,16 +1,26 @@
-import time
-
+import random
 import tweepy
 from config import create_api
+
+Messages = ["You got this Sugar Bear",
+    "That is a gun in my pocket, but I am happy to see you.",
+    "Will you be my Kirsten Dunst?",
+    "Put on your positive pants Sugar Bear",
+    "Drink some coffee, put on some gansta rap and handle it",
+    "Oh, yes you certainly fucking can",
+    "Chin up princess or the crown slip",
+    "It’s just a flesh wound",
+    "I learned a long time ago that worrying is like a rocking chair. It gives you something to do but it doesn’t get you anywhere"]
+
+def wingman(api):
+    user_id = "307187231"
+    message = random.choice(Messages)
+    api.update_status(f"@{user_id}, {message}")
 
 
 def main():
     api = create_api()
 
-def wingman(api):
-    user = api.user_timeline(user_id = "307187231", count = 1)
-    print(user)
-    return user
 
 if __name__ == "__main__":
     main()
