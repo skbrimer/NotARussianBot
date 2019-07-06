75b67ec413355088a255e049c70716846dd100a1
skbrimer <skbrimer@gmail.com>
2019-07-06 10:06:36 -0500

    Adding clock file for cron job on Heroku

--- clock.py
+++ clock.py
@@ -2,8 +2,8 @@ from apscheduler.schedulers.blocking import BlockingScheduler
 
 sched = BlockingScheduler()
 
-@sched.scheduled_job('cron', day_of_week='mon-sun', hour=7)
+@sched.scheduled_job('cron', day_of_week='mon-fri', hour=7)
 def scheduled_job():
-    print('This job is run every day at 7am.')
+    python wingman.py
 
 sched.start()
