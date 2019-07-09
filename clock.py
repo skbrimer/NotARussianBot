792f4b8ae8e7ddcf7b1935a78bc43f385e1ab6c4
skbrimer <skbrimer@gmail.com>
2019-07-09 08:49:06 -0500

    corrected apscheduler

--- clock.py
+++ clock.py
@@ -1,9 +1,10 @@
 from apscheduler.schedulers.blocking import BlockingScheduler
+import wingman
 
 sched = BlockingScheduler()
 
 @sched.scheduled_job('cron', day_of_week='mon-fri', hour=7)
 def scheduled_job():
-    wingman.py
+    wingman()
 
 sched.start()
